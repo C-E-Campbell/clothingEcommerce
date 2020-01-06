a19630bcf6a9b8ab149fb60b42c9f91c4ca5a0ab
charles <codelyfewithcharlie@gmail.com>
2020-01-06 01:28:09 -0500

    upload

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -4,7 +4,7 @@ import "./homepage.styles.scss";
 const HomePage = props => {
   return (
     <div clasName="homepage">
-      <div className="direactory-menu">
+      <div className="directory-menu">
         <div className="menu-item">
           <div className="content">
             <h1 className="title">HATS</h1>
