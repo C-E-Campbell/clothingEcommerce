ecda26cab41cb6ddf9020c797c2ca8224f3ae4d2
charles <codelyfewithcharlie@gmail.com>
2020-01-06 16:21:08 -0500

    added components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../components/Directory/Directory";
+
+const HomePage = props => {
+  return (
+    <div clasName="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
