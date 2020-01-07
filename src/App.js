4548c20cc6f0cae3a7ae14a4417bf6aaa86697a2
charles <codelyfewithcharlie@gmail.com>
2020-01-06 23:39:16 -0500

    added logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePage from "./pages/Homepage/Homepage.jsx";
 import Shop from "./pages/Shop/Shop.jsx";
+import Header from "./components/Header/Header.jsx";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./header.styles.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../Assets/crown.svg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+      </div>
+    </header>
+  );
+};
+export default Header;
